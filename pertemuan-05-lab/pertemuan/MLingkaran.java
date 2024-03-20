5f59d94adbbdfb23afa6d408faee10e7e6a6b33c
Farrel Amirta Irbah <farrelamirtairbah27@gmail.com>
2024-03-20 11:23:47 +0700

    Pengumpulan Tugas Modul Week 5 PBO D2

---
+++ pertemuan-05-lab/pertemuan/MLingkaran.java
@@ -0,0 +1,6 @@
+public class MLingkaran {
+    public static void main(String[] args) {
+        Lingkaran L = new Lingkaran(10.2);
+        System.out.println("Luas Lingkaran dengan" + "jejari 10.2 satuan adalah " + L.hitungLuas());
+    }
+}
